a8e1088c33b8f9e48336b9ba0796830f7fa2ee55
Darlan Thiago <tdarlan1@hotmail.com>
2021-04-15 10:42:38 -0300

    Update Auth Context to Update User

--- src/contexts/hooks/AuthContext/index.js
+++ src/contexts/hooks/AuthContext/index.js
@@ -1,4 +1,10 @@
-import React, { createContext, useCallback, useContext, useState } from "react";
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 import Cookies from "js-cookie";
@@ -11,50 +17,50 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-  const [data, setData] = useState(async () => {
-    const token = localStorage.getItem("@RJSAuth:token");
-    const user = localStorage.getItem("@RJSAuth:user");
+  const [user, setUser] = useState({});
 
-    if (!token || !user) {
-      // history.push("/login");
-      setLoading(false);
-      return;
-    }
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem("@RJSAuth:token");
+      const user = localStorage.getItem("@RJSAuth:user");
 
-    setLoading(true);
-    //check token is valid
-
-    await api
-      .get("/api/user/me", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((resp) => {
-        if (resp.data) {
-          setData({
-            token,
-            user: JSON.parse(user),
-          });
-          api.defaults.headers["Authorization"] = `Bearer ${token}`;
-          setLoading(false);
-          return;
-        }
-
+      if (!token || !user) {
         setLoading(false);
-        history.push("/login");
-      })
-      .catch((error) => {
-        localStorage.removeItem("@RJSAuth:token");
-        localStorage.removeItem("@RJSAuth:user");
-        delete api.defaults.headers.common["Authorization"];
-        delete api.defaults.headers.common["XSRF-TOKEN"];
-        Cookies.remove("XSRF-TOKEN");
-        setLoading(false);
-        toast.error("❌ Ops! Faça login novamente!");
         return;
-      });
-  });
+      }
+
+      setLoading(true);
+      //check token is valid
+
+      await api
+        .get("/api/user/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((resp) => {
+          if (resp.data) {
+            setUser(resp.data);
+            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+            setLoading(false);
+            return;
+          }
+
+          setLoading(false);
+          history.push("/login");
+        })
+        .catch((error) => {
+          localStorage.removeItem("@RJSAuth:token");
+          localStorage.removeItem("@RJSAuth:user");
+          delete api.defaults.headers.common["Authorization"];
+          delete api.defaults.headers.common["XSRF-TOKEN"];
+          Cookies.remove("XSRF-TOKEN");
+          setLoading(false);
+          toast.error("❌ Ops! Faça login novamente!");
+          return;
+        });
+    })();
+  }, [history]);
 
   const signIn = useCallback(
     async (email, password) => {
@@ -74,7 +80,7 @@ export const AuthProvider = ({ children }) => {
           localStorage.setItem("@RJSAuth:user", JSON.stringify(user));
           localStorage.setItem("@RJSAuth:token", token);
 
-          setData({ token, user });
+          setUser(user);
           setLoading(false);
 
           history.push("/");
@@ -84,7 +90,7 @@ export const AuthProvider = ({ children }) => {
           return;
         })
         .catch((error) => {
-          setData({});
+          setUser({});
           setLoading(false);
           toast.error(
             "❌ Erro no login, verifique os dados e tente novamente!"
@@ -106,7 +112,7 @@ export const AuthProvider = ({ children }) => {
         delete api.defaults.headers.common["XSRF-TOKEN"];
         Cookies.remove("XSRF-TOKEN");
 
-        setData({});
+        setUser({});
         setLoading(false);
         history.push("/login");
         toast.success("✅ Logout feito com sucesso!");
@@ -117,7 +123,7 @@ export const AuthProvider = ({ children }) => {
         delete api.defaults.headers.common["Authorization"];
         delete api.defaults.headers.common["XSRF-TOKEN"];
         Cookies.remove("XSRF-TOKEN");
-        setData({});
+        setUser({});
         setLoading(false);
         toast.error("❌ Ops, algo deu errado! Faça o login novamente.");
       });
@@ -125,7 +131,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider
-      value={{ signed: !!data.user, user: data.user, signIn, signOut, loading }}
+      value={{ signed: !!user, user, setUser, signIn, signOut, loading }}
     >
       {children}
     </AuthContext.Provider>
